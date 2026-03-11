ee3497973563218aac6c66bbc740f8a9c26e663d
chaewookim <chaewookim2877@gmail.com>
2026-03-11 17:46:57 +0900

    [fix] #37 공장 발주 API URI 수정

--- src/api/factoryOrders.js
+++ src/api/factoryOrders.js
@@ -2,7 +2,7 @@ import { request } from './http.js'
 
 const BASE = '/api/v1/factory/orders'
 
-export const getOrderList = (isAll) => request(`${BASE}/accepted?isAll=${isAll}`)
+export const getOrderList = (isAll) => request(`${BASE}?isAll=${isAll}`)
 export const updateOrders = (data) => request(`${BASE}?isAccept=${data.isAccept}`, {
   method: 'PATCH',
   headers: { 'Content-Type': 'application/json' },
